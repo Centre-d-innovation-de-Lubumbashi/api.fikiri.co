746f60e4f98041c5045d5a0c3798ab03fd7bda66
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-10 15:47:54 +0200

    feat: crate call dto

---
+++ src/calls/dto/create-call.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateCallDto {
+  @IsNotEmpty({ message: 'Le nom ne peut pas être vide' })
+  name: string;
+
+  @IsNotEmpty({ message: 'La description ne peut pas être vide' })
+  description:  string;
+
+  @IsNotEmpty({ message: 'La date de début ne peut pas être vide' })
+  startedAt: Date;
+
+  @IsNotEmpty({ message: 'La date de fin ne peut pas être vide' })
+  endedAt: Date;
+}
