c39a971b30c2c57bbe05b010c7b48a12544fe533
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-10 16:07:47 +0200

    feat: use createDto and updateDto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,7 +10,6 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Prisma } from '@prisma/client';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { v4 as uuidv4 } from 'uuid';
