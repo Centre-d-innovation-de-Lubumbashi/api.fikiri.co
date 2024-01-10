fb47e14eac9f9f4c9a2995643e6b185d9aa421a2
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-10 15:55:14 +0200

    feat: crate update DTO

---
+++ src/calls/dto/update-call.dto.ts
@@ -0,0 +1,5 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCallDto } from './create-call.dto';
+
+export class UpdateCallDto extends PartialType<CreateCallDto>(CreateCallDto) {
+}
