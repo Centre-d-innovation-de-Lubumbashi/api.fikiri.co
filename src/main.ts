362f294ca1216a5dc2aa7563d69373e1f947668d
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-11 14:53:25 +0200

    feat: sessions and passport initialization

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import * as passport from 'passport';
+import * as session from 'express-session';
 
 const port = Number(process.env.PORT) as number;
 
@@ -11,7 +13,16 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
   });
-
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: Boolean(process.env.SESSION_RESAVE),
+      saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
+      cookie: { maxAge: +process.env.SESSION_COOKIE_MAX_AGE, secure: false },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.useGlobalPipes(new ValidationPipe({
     exceptionFactory: (errors) => {
       const result = errors.map((error) => ({
