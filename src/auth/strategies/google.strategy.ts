f2c7dd5399c97e4d509eae9e07a8bbbe369ed678
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-18 19:30:59 +0200

    feat: create the user if he does not exist after login

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -2,28 +2,32 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { UsersService } from 'src/users/users.service';
+import { CreateWithGoogleDto } from '../../users/dto/create-with-google.dto';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 
-  constructor(readonly configService: ConfigService) {
+  constructor(
+    private readonly userService: UsersService,
+    readonly configService: ConfigService,
+  ) {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_SECRET'),
-      callbackURL: 'http://localhost:8000/auth/google/redirect',
+      callbackURL: configService.get('GOOGLE_REDIRECT_URI'),
       scope: ['email', 'profile'],
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
-    const { name, emails, photos } = profile;
-    const user = {
+  async validate(_accessToken: string, _refreshToken: string, profile: any, done: VerifyCallback) {
+    const { emails, name, photos } = profile;
+    const userDto: CreateWithGoogleDto = {
       email: emails[0].value,
       name: `${name.givenName} ${name.familyName}`,
-      profile: photos[0].value,
-      accessToken,
-      refreshToken,
+      googleImage: photos[0].value,
     };
+    let user = await this.userService.findOrCreate(userDto);
     done(null, user);
   }
 }
