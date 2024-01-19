678c7ef1c6044badd0368e24ff2e63bf60c70fd2
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-19 15:02:16 +0200

    feat: delete migrations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -117,7 +117,7 @@ export class UsersService {
           ...createWithGoofleDto,
           roles: {
             connect: {
-              id: 4,
+              name: 'user'.toUpperCase(),
             },
           },
         },
