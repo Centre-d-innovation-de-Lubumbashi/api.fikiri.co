2201d2ef3c085e21517f14c9c99bcc3baf45a97c
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-19 14:23:47 +0200

    feat: delete migrations

--- prisma/migrations/20240110114952_make_fiels_optionals/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE `User` MODIFY `password` VARCHAR(191) NULL,
-    MODIFY `phoneNumber` VARCHAR(191) NULL,
-    MODIFY `address` VARCHAR(191) NULL;
--- prisma/migrations/20240115081625_add_google_image/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Solution` ADD COLUMN `imageLink` VARCHAR(191) NULL;
-
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `googleImage` VARCHAR(191) NULL;
