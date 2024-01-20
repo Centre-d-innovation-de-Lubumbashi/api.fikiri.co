df7ed4412aba5309ac1d00795d3cf39d475197f3
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-20 16:09:33 +0200

    refactor: drop login dto

--- src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, MinLength } from 'class-validator';
-
-export class LoginDto {
-  @IsEmail({}, { message: "L'email saisi est invalide" })
-  email: string;
-
-  @MinLength(4, { message: "Le mot de passe doit containir au-moins 4 caractères" })
-  password: string;
-}
