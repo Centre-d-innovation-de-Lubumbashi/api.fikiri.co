07aa50fa09f275d29aa40f471fb19ab3d1a9655d
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-26 12:43:38 +0200

    chore: allow cors form fikiri and admin.fikiri

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,18 @@ const port = Number(process.env.PORT) as number;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:4000',
+      'https://fikiri.co',
+      'https://www.fikiri.co',
+      'https://admin.fikiri.co',
+      'https://www.admin.fikiri.co',
+    ],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
-    // headers exposed to the client
     exposedHeaders: ['Authorization'],
     credentials: true,
-    optionsSuccessStatus: 204
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   });
   app.use(
     session({
