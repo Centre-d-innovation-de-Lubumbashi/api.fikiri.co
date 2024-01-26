bfd9b22d1d3cde6fa675651257e052ec7259438e
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-26 13:16:22 +0200

    chore: allow cors form fikiri and admin.fikiri

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,10 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     exposedHeaders: ['Authorization'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    optionsSuccessStatus: 204
   });
   app.use(
     session({
