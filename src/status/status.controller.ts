00158c6d29ed72a4e929ec1827c2bf2e9fa89f35
musanzi <121388088+musanziw@users.noreply.github.com>
2024-01-31 13:29:42 +0200

    Update status.controller.ts

--- src/status/status.controller.ts
+++ src/status/status.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { StatusService } from './status.service';
 import { UpdateStatusDto } from './dto/update-status.dto';
 import { CreateStatusDto } from './dto/create-status.dto';
