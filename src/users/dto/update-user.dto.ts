d0f7f897ba81a9a8fcbd2291538784b5da5465b1
musanzi <121388088+musanziw@users.noreply.github.com>
2024-01-31 12:45:12 +0200

    fix: update update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,7 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import CreateUserDto from './create-user.dto';
 import { IsNotEmpty } from 'class-validator';
 
-export class UpdateUserDto extends PartialType<CreateUserDto>(CreateUserDto) {
-  @IsNotEmpty({ message: 'le status est obligatoire' })
-  status: number;
-}
+export class UpdateUserDto extends PartialType<CreateUserDto>(CreateUserDto) { }
