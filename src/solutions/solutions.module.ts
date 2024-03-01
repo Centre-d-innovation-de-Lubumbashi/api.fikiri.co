f58f751b08ab8619ed1d6154274459aaef4b7dfe
Musanzi Wilfried <musanziw.m@gmail.com>
2024-03-01 09:30:55 +0200

    chore: import newly created services and controllers

--- src/solutions/solutions.module.ts
+++ src/solutions/solutions.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { SolutionsService } from './solutions.service';
 import { SolutionsController } from './solutions.controller';
 import { FeedbacksModule } from 'src/feedbacks/feedbacks.module';
+import { SolutionsFeedbacksService } from './solutions-feebacks.service';
+import { SolutionsFeedbacksController } from './solutions-feebacks.controller';
+import { SolutionsFiltersController } from './solutions-filters.controller';
+import { SolutionsFiltersService } from './solutions-filters.service';
 
 @Module({
   imports: [FeedbacksModule],
-  controllers: [SolutionsController],
-  providers: [SolutionsService],
+  controllers: [
+    SolutionsController,
+    SolutionsFeedbacksController,
+    SolutionsFiltersController,
+  ],
+  providers: [
+    SolutionsService,
+    SolutionsFeedbacksService,
+    SolutionsFiltersService,
+  ],
 })
 export class SolutionsModule {}
