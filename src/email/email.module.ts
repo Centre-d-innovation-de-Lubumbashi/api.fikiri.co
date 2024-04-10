345140b79a66ce6a25cd86430ec062abf0238207
Musanzi Wilfried <musanziw.m@gmail.com>
2024-04-10 09:34:50 +0200

    feat: add handlebars template

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,9 +1,10 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
 
-@Global()
 @Module({
   imports: [
     MailerModule.forRootAsync({
@@ -11,11 +12,21 @@ import { ConfigService } from '@nestjs/config';
       useFactory: (config: ConfigService) => ({
         transport: {
           host: config.get('MAIL_HOST'),
-          secure: false,
+          secure: true,
           port: Number(config.get('MAIL_PORT')),
           auth: {
             user: config.get('MAIL_USERNAME'),
             pass: config.get('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `Suport fikiri <${config.get('MAIL_USERNAME')}>`,
+        },
+        template: {
+          dir: join(__dirname, '..', 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
           },
         },
       }),
