39157e79038d9130f15460d23de7f69bf65a7c0d
Musanzi Wilfried <musanziw.m@gmail.com>
2024-04-12 09:43:08 +0200

    feat: create a separated route for update password

--- src/auth/dto/update-password.dto.ts
+++ src/auth/dto/update-password.dto.ts
@@ -1,8 +1,17 @@
-import { MinLength } from 'class-validator';
+import { IsNotEmpty, MinLength } from 'class-validator';
+import { Match } from '../decorators/match.decorator';
 
 export class UpdatePasswordDto {
+  @IsNotEmpty({ message: 'L\'ancien mot de passe est recquis' })
+  old_password: string;
+
   @MinLength(6, {
     message: 'Le mot de passe doit contenir au moins 6 caractères',
   })
   password: string;
+
+
+  @IsNotEmpty({ message: 'La confirmation du mot de passe est recquise' })
+  @Match('password', { message: 'Les mots de passe ne correspondent pas' })
+  password_confirm: string;
 }
