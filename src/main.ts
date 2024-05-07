ede848f337aa8f634dcbc5bc077f90296447940f
Musanzi Wilfried <musanziw.m@gmail.com>
2024-05-07 10:38:27 +0200

    feat: set sameCookies strict

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import * as passport from 'passport';
 import * as session from 'express-session';
 
-const port = Number(process.env.PORT) as number;
+const port: number = Number(process.env.PORT) as number;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,7 +19,7 @@ async function bootstrap() {
       secret: process.env.SESSION_SECRET,
       resave: Boolean(process.env.SESSION_RESAVE),
       saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
-      cookie: { maxAge: +process.env.SESSION_MAX_AGE, secure: false }
+      cookie: { maxAge: +process.env.SESSION_MAX_AGE, secure: false, sameSite: 'strict' }
     })
   );
   app.use(passport.initialize());
@@ -38,4 +38,4 @@ async function bootstrap() {
   await app.listen(port);
 }
 
-bootstrap().then(() => console.log(`🚀 Application is running on: localhost:${port}`));
+bootstrap().then();
