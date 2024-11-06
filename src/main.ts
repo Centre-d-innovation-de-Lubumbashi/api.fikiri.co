6a36e6f50270cd9504a11fb9c22700516133afb9
Wilfried Musanzi <musanziw.m@gmail.com>
2024-11-06 09:49:13 +0200

    chore: remove unecessary code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import * as passport from 'passport';
 import * as session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 
 const port: number = Number(process.env.PORT) as number;
 
@@ -10,7 +10,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    allowedHeaders: ['Content-Type'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
   });
@@ -19,22 +18,12 @@ async function bootstrap() {
       secret: process.env.SESSION_SECRET,
       resave: Boolean(process.env.SESSION_RESAVE),
       saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
-      cookie: { maxAge: 86_400_000, secure: false, sameSite: 'strict', httpOnly: true }
+      cookie: { maxAge: 86400000, secure: false, sameSite: 'strict', httpOnly: true }
     })
   );
   app.use(passport.initialize());
   app.use(passport.session());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      exceptionFactory: (errors) => {
-        const result = errors.map((error) => ({
-          property: error.property,
-          message: error.constraints[Object.keys(error.constraints)[0]]
-        }));
-        return new BadRequestException(result);
-      }
-    })
-  );
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
 
